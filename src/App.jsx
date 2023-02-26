14d00450d469ee15163a4545ae4ad3a3ef798a9a
Jerome Doran <disneyfan9184@gmail.com>
2023-02-25 19:46:00 -0500

    added chakra ui library for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-function App() {
-
-  return (
-    <div>
-      Hello world!
-    </div>
-  )
+export default function App() {
+  return <div>Hello world!</div>;
 }
-
-export default App
---
+++ src/pages/myGigs.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MyGigs() {
+  return <div> MyGigs</div>;
+}
